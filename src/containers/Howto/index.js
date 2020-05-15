ec6d03fe7c7a140279194e5f49e8a4e87836dc80
ys-sudo <georgios.lazaridis0@gmail.com>
2020-05-15 11:47:02 +0300

    rename howto

--- src/containers/Howto/index.js
+++ src/containers/Howto/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default class Howto extends React.Component {
+export default class Index extends React.Component {
 	render () {
 		return (
 			<div>
