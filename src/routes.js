89355900ba8a7a5b51440b4009ff4c55c96d0022
ys-sudo <georgios.lazaridis0@gmail.com>
2020-05-15 11:53:59 +0300

    try

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import App from './containers/App';
-import Howto from './containers/Howto';
+import Howto from './containers/Howto/index';
 
 export default () => {
 	return (
 		<BrowserRouter>
 			<Switch>
 			  <Route exact path='/' component={App}/>
-			  <Route path='/Howto' component={Howto}/>
+			  <Route path='/Howto/' component={Howto}/>
 			</Switch>
 		</BrowserRouter>
 	)
